dc4c8a93aa3dad389cdc9ac8fb66413f17c197db
児玉継兵 <tsugihei@heyMac.local>
2020-07-29 17:17:11 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'top#index'
+
+  resources :list, only: %i(new create edit update destroy) do
+    # resources :card, only: %i(new create show edit update destroy)
+    resources :card, except: %i(index)
+  end
+end
